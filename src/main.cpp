1f25e45366a99ac8c4e8619e4f03b2420589367c
vertoker <vertogpro@gmail.com>
2024-07-07 15:57:27 +0500

    remove console

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main() {
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    std::cout << extensionCount << " extensions supported\n";
+    //std::cout << extensionCount << " extensions supported\n";
 
     glm::mat4 matrix;
     glm::vec4 vec;
