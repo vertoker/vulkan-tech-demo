c3d2ebb2906dbd659a3a9d7214a0e380aa7fc158
vertoker <vertogpro@gmail.com>
2024-07-09 14:09:28 +0500

    copy default vk device create script

--- src/render/window.cpp
+++ src/render/window.cpp
@@ -1,5 +1,8 @@
 #include "window.h"
 
+// std
+#include <stdexcept>
+
 VulkanWindow::VulkanWindow(const int& w, const int& h, const std::string& name)
 {
 	this->width = w;
@@ -18,4 +21,11 @@ VulkanWindow::~VulkanWindow()
 {
 	glfwDestroyWindow(window);
 	glfwTerminate();
+}
+
+void VulkanWindow::createWindowSurface(VkInstance instance, VkSurfaceKHR* surface)
+{
+	if (glfwCreateWindowSurface(instance, window, nullptr, surface) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create window surface");
+	}
 }
